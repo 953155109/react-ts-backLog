7dd65bd6ca96d09a70dd1907ce47c07719aa90ed
EVE <953155109@qq.com>
2023-05-29 16:36:00 +0800

    feat：添加组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: EVE
+ * @Date: 2023-04-07 15:08:20
+ * @LastEditors: EVE
+ * @LastEditTime: 2023-05-28 17:41:30
+ */
+import React from "react"
+import "./App.css"
+import FilterableProductTable from "./components/FilterableProductTable"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilterableProductTable />
     </div>
-  );
+    // <div className="App">
+    //   <header className="App-header">
+    //     <div className='top'>Hello world1</div>
+    //     <div className='midden'>Hello world2</div>
+    //     <div className='bottom'>Hello world3</div>
+    //   </header>
+    // </div>
+  )
 }
 
-export default App;
+export default App
